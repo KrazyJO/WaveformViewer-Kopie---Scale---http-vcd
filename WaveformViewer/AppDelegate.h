c97b126b97129ca270f57773ce31004bcd9d47ba
student <student@frogs.digi.e-technik.uni-kassel.de>
2013-01-31 10:22:20 +0100

    Initial Commit

---
+++ WaveformViewer/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  WaveformViewer
+//
+//  Created by student on 31.01.13.
+//  Copyright (c) 2013 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ WaveformViewer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WaveformViewer
+//
+//  Created by student on 31.01.13.
+//  Copyright (c) 2013 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
