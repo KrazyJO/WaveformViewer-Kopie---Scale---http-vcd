e8b9a3bb8c1a6db1815b289c5a2d144b5f80432d
KrazyJO <joern.otte@gmx.net>
2013-03-20 18:13:47 +0200

    test commit

--- WaveformViewer/ViewController.m
+++ WaveformViewer/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    //test
 	// Do any additional setup after loading the view, typically from a nib.
     [self.tableView setDelegate:self];
     [self.tableView setDataSource:self];
